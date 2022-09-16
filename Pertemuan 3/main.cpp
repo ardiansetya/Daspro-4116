87cc41d577ba0cdb33ce1d9dc80fe345bd160cc4
ardiansetya <113000649+ardiansetya@users.noreply.github.com>
2022-09-16 23:30:41 +0700

    Add files via upload

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,47 @@
+//Tugas daspro pertemuan 3
+/*  nama    : Ardian Setya Pradana
+    NIM     : A11.2022.14596
+    kelas   : 4116 */
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    string n = "Ardian Setya Pradana";
+    int u = 19;
+    float t = 175.23;
+    char a = 'L';
+
+
+    cout << "nama saya " << n;
+    cout << endl;
+    cout << "saya berumur " << u;
+    cout << endl;
+    cout << "tinggi saya " << t;
+    cout << endl;
+    cout << "ukuran baju saya " << a;
+    cout << endl;
+    cout << endl;
+    printf("ini pakai printf");
+
+    string y;
+    long int x;
+    cout << endl;
+    cout << endl;
+    cout << "masukan nama : ";
+    cin >> y;
+    cout << "halooo " << y;
+    cout << endl;
+    cout << "\nmasukan nomor : ";
+    cin >> x;
+    cout << "nomor kamu " << x;
+
+
+    return 0;
+}
+
+
+
